e8d19a7254f304776e4ec3c1d9eade0d769ca367
sweta <sweta.koski@metropolia.fi>
2023-10-10 11:55:37 +0300

    #module 2 exercises

---
+++ main.py
@@ -0,0 +1,40 @@
+#
+# name=input("Enter your name : ")
+# if name== ("Viivi"):
+#     print ("Hello, " + "Viivi" + "!")
+# if name==("Ahmed"):
+#     print("Hello, "+ "Ahmed" + " ! ")
+#
+#
+#
+# from math import pi
+# r= float(input("Enter radius of a circle:"))
+# print ("The area of a circle with radius " + str(r) + " is " + str(pi*r**2))
+# """
+#
+# """
+# l=int(input("Enter the lenght of a rectangle: "))
+# w=int(input("Enter the width of a rectangle: "))
+# print ("The area of a rectangle is " + str(l*w)+ ".")
+# print("The perimeter of a rectangle is " + str(l*4) +".")
+# """
+#
+#
+# """
+# n1=int(input("Enter the first number: "))
+# n2=int(input("Enter your second number: "))
+# n3=int(input("Enter your third number: "))
+# d=int(n1+n2+n3)/3
+# print("The sum is " + str(n1+n2+n3) + ".")
+# print("The product is " + str(n1*n2*n3) + ".")
+# print ("The average is " + str(d))
+# """
+# """
+# t=float(input("Enter your mass in talents: "))
+# p=float(input("Enter you mass in pounds :"))
+# l=float(input("Enter your mass in lots: "))
+# t_g= float((t * 8512))
+# p_g = float((p * 425.6))
+# l_g= float((l * 13.3))
+# print("Result: " + str((t_g+p_g+l_g)/1000))
+# print("The weight in modern unit: " + "29 kilograms " + "and " + "545.95 grams" + ".")
